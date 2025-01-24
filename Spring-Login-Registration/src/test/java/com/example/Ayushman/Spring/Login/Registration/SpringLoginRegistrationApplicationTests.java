d8e663ee5540a613990975b5508d2b8df025ce28
ayush <ayushmangiri@gmail.com>
2025-01-24 12:41:20 +0530

    first commit

---
+++ Spring-Login-Registration/src/test/java/com/example/Ayushman/Spring/Login/Registration/SpringLoginRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ayushman.Spring.Login.Registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoginRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
