e6b35a631adaa34d97b83e657e556b21c99dccbe
ayush <ayushmangiri@gmail.com>
2025-01-28 18:00:23 +0530

    2nd login  commit

---
+++ Spring-Login-Registration/src/main/java/com/example/Ayushman/Spring/Login/Registration/Student.java
@@ -0,0 +1,47 @@
+package com.example.Ayushman.Spring.Login.Registration;
+
+public class Student {
+    private int id;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", marks=" + marks +
+                '}';
+    }
+
+    public Student(int id, int marks, String name) {
+        this.id = id;
+        this.marks = marks;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+    private int marks;
+}
